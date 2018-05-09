e35ad476809cd1a2349595e4654b9539c001a066
Greg Barnes <gregdbarnes@gmail.com>
2018-05-09 16:49:43 +0100

    Provide fallback when access token is unavailable

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-// const bodyParser = require('body-parser');
 const serveIndex = require('serve-index');
 const path = require('path');
 const gulpTasks = require('./routes/gulp-tasks');
@@ -14,14 +13,12 @@ app.use(
   serveIndex(path.join(__dirname, 'build', 'assets'), { icons: true, view: 'details' })
 );
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')));
 // app.get('/ping', (req, res) => res.send('pong'));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')));
 app.get('/gulp', gulpTasks.gulp);
 app.get('/archive', archive.zip);
 app.get('/download', archive.download);
 
 app.set('port', process.env.PORT || 8080);
 app.get('/port', (req, res) => res.send(`${app.get('port')}`));
-
-// const port = process.env.PORT || 8080;
 app.listen(app.get('port'), () => console.log(`Server listening on port ${app.get('port')}`));
--- src/helper/functions.js
+++ src/helper/functions.js
@@ -1,7 +1,10 @@
 import packageJson from './../../package.json';
 
 export const fetchGithubVersion = url => {
-  return fetch(url + '?access_token=' + process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN)
+  const githubAccessToken = process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+    ? `?access_token=${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`
+    : '';
+  return fetch(url + githubAccessToken)
     .then(response => response.json())
     .then(versionData => atob(versionData.content).trim());
 };
